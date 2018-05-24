b010cbf23f2dd3dc671f1636197454fada52c2a9
Tim66262 <tim.damato@swisscom.com>
2018-05-24 15:01:29 +0200

    Bug

--- php/appl_functions.php
+++ php/appl_functions.php
@@ -18,5 +18,19 @@ function login() {
  * Beinhaltet die Anwendungslogik zur Registration
  */
 function registration() {
+    setValue("phpmodule", $_SERVER['PHP_SELF']."?id=".getValue("func"));
+    return runTemplate( "../templates/".getValue("func").".htm.php" );
+    $meldung = "";
+    $email = "";
+    $passwort = "";
+    if (isset($_POST['email']) && isset($_POST['passwort'])) {
+            $_SESSION['uid'] = $uid;
+            $entryId=getMaxEntryId($uid);
+            header("Location:index.php?function=entries_private&uid=$uid&eid=$entryId");
+
+            $meldung = "Login-Daten nicht richtig... bitte nochmals versuchen oder registriere dich.";
+            $email = $_POST['email'];
+            $passwort = $_POST['passwort'];
+    }
 }
 ?>
